a824c7afde6b7596210340ef5f6768bc91feafb8
Marco Prestes <mmaciel@stackbuilders.com>
2022-01-26 15:07:13 -0300

    Removed all references from vectorized.io URLs

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,7 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
+    <Layout 
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
