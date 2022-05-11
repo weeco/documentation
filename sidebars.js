20e943f2fe6d26e70ba33e363a9a120481b9dae6
Paulo Borges <paulohtb@hotmail.com>
2022-05-11 20:46:01 -0300

    docs: Apply suggestions from code review for 22.1
    Co-authored-by: Joyce Fee <102751339+Feediver1@users.noreply.github.com>
    
    Update docs/data-management/tiered-storage.mdx
    
    Co-authored-by: Joyce Fee <102751339+Feediver1@users.noreply.github.com>
    
    from: we recommend to: it is recommended
    
    add arbitrary config back to sidebar

--- sidebars.js
+++ sidebars.js
@@ -103,6 +103,12 @@
               label: 'Using configuration parameters with a custom resource',
               id: 'deployment/kubernetes-additional-config',
             },
+            {
+              type: 'doc',
+              label: 'Arbitrary configuration',
+              id: 'deployment/arbitrary-configuration',
+            },
+            
           ]
         },
         'deployment/performance-storage-tuning',
