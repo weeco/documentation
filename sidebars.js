de51513adaa66f25f9f61604b5dd2c3f697fc7e8
Paulo Borges <paulohtb@hotmail.com>
2023-02-01 12:15:09 -0300

    docs: remove rpk redpanda tune help (#978)

--- sidebars.js
+++ sidebars.js
@@ -768,7 +768,6 @@ module.exports = {
                   },
                   items: [
                     "reference/rpk/rpk-redpanda/rpk-redpanda-tune",
-                    "reference/rpk/rpk-redpanda/rpk-redpanda-tune-help",
                     "reference/rpk/rpk-redpanda/rpk-redpanda-tune-list",
                   ],
                 },
