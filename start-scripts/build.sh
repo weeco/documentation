add1af837134a2f8d6e318b962baa8e32595db72
JakeSCahill <jake@redpanda.com>
2023-06-05 14:36:26 +0100

    Do not set algolia variables in the script
    Let Netlify handle those

--- start-scripts/build.sh
+++ start-scripts/build.sh
@@ -3,12 +3,6 @@
 # Get the latest Redpanda Console version
 export CONSOLE_VERSION=$(node start-scripts/getLatestConsoleVersion.js);
 
-# Set the ALGOLIA environment variables
-export ALGOLIA_API_KEY=0a3bd3d7dcdcd4f73c646dcd0c3c5252;
-export ALGOLIA_APP_ID=0HC7T0GI45;
-export ALGOLIA_INDEX_NAME=test;
-export ALGOLIA_ADMIN_API_KEY=;
-
 # Set the latest-console-version attribute
 if [[ -n "$CONSOLE_VERSION" ]]; then
   attribute="--attribute latest-console-version=$CONSOLE_VERSION"
