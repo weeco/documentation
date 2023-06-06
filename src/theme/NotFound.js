6a033509b69f601e75b365170400a8cce50d8d07
Paulo Borges <paulohtb@hotmail.com>
2023-06-06 15:20:26 -0300

    engine: add smart redirect to versioned content (#1655)

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -4,7 +4,11 @@ import { PageMetadata } from "@docusaurus/theme-common";
 import Layout from "@theme/Layout";
 import styles from "./styles.module.css";
 import sidebar from "../../sidebars"
-
+import sidebarv21_11 from "../../versioned_sidebars/version-21.11-sidebars.json"
+import sidebarv22_1 from "../../versioned_sidebars/version-22.1-sidebars.json"
+import sidebarv22_2 from "../../versioned_sidebars/version-22.2-sidebars.json"
+import sidebarv22_3 from "../../versioned_sidebars/version-22.3-sidebars.json"
+import sidebarv23_2 from "../../versioned_sidebars/version-23.2-sidebars.json"
 
 let hasSmartRedirected = false;
 let smartRedirectPath;
@@ -58,22 +62,67 @@ function mapPathFileToCompleteURL(docIdsArray) {
   return map;
 }
 
-const docIdsArray = getDocIds(sidebar.docsSidebar);
-const urlMap = mapPathFileToCompleteURL(docIdsArray);
+function hasVersionNumber(url) {
+  const segments = url.split('/');
+  for (let segment of segments) {
+    // Remove any URL encoding
+    segment = decodeURIComponent(segment);
+    // Check if the segment is a valid version number. Example 21.11
+    if (/^\d+(\.\d+)*$/.test(segment)) {
+      return segment;
+    }
+  }
+  return null;
+}
 
-function findCloseURL (url){
+function findCloseURL (url,versionNumber){
+  let docIdsArray;
+  const versionedSidebars = new Map([
+    ["21.11", sidebarv21_11],
+    ["22.1", sidebarv22_1],
+    ["22.2", sidebarv22_2],
+    ["22.3", sidebarv22_3],
+    ["23.2", sidebarv23_2],
+  ]);
+  //if it has a version number, extract the sidebar file of that version. If it can't find, throw version not found.
+  if(versionNumber){
+    if (versionedSidebars.has(versionNumber)) {
+      docIdsArray = getDocIds(versionedSidebars.get(versionNumber).docsSidebar);
+    }
+    else{
+      throw new Error("Version not found");
+    }
+  }
+  else{
+    docIdsArray = getDocIds(sidebar.docsSidebar);
+  }
+  const urlMap = mapPathFileToCompleteURL(docIdsArray);
   return urlMap.get(url);
 }
 
 function smartRedirect(){
   const originalPathName = window.location.pathname;
+  const versionNumber = hasVersionNumber(originalPathName);
   const segments = originalPathName.split('/').filter(Boolean);
   const lastString = segments[segments.length - 1];
-  const closeURL = findCloseURL(lastString);
   const prefix = window.location.origin + '/docs/';
-  possibleRedirect = prefix + closeURL;
-  const sessionStorageKey = 'smartRedirectHasRun_'+lastString;
-  
+  let closeURL;
+  let sessionStorageKey;
+  if (versionNumber!==null) {
+    try{
+      closeURL=findCloseURL(lastString,versionNumber);
+      possibleRedirect = prefix + versionNumber +'/'+ closeURL;
+      sessionStorageKey= 'smartRedirectHasRun_'+versionNumber+'_'+lastString;
+    }
+    catch (error){
+      return null;
+    }
+  }
+  else{
+    closeURL = findCloseURL(lastString);
+    possibleRedirect = prefix + closeURL;
+    sessionStorageKey = 'smartRedirectHasRun_'+lastString;
+  }
   const hasRun = sessionStorage.getItem(sessionStorageKey);
   if(hasRun === null){
     sessionStorage.setItem(sessionStorageKey, true);
